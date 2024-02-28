97c71094198a6b4884c0d9d416b5eb370a3cb0f2
anabarcari <112519061+anabarcari@users.noreply.github.com>
2024-02-28 14:07:13 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {chunkSizeWarningLimit: 1600,},
+  base: '/ThunderFixApp/',
   // other configurations...
+    
 
   assetsInclude: ['**/*.png', '**/*.PNG'],
 

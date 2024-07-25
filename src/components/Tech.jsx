25a1cb01ad73f5effd77d3bb8f92904ec29b08ab
anabarcari <ana.barcari85@myhunter.cuny.edu>
2024-07-24 22:45:14 -0400

    updated images

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,7 +7,7 @@ import Img1 from '../assets/imgs/lexus_before2.jpg';
 import Img2 from '../assets/imgs/lexus_after.jpg';
 import Img3 from '../assets/imgs/4.jpg';
 import Img4 from '../assets/imgs/14.jpg';
-import Img5 from '../assets/imgs/11.jpg';
+
 import Img6 from '../assets/imgs/21.jpg';
 import Img7 from '../assets/imgs/23.jpg';
 import Img8 from '../assets/imgs/30.jpg';
@@ -16,10 +16,23 @@ import Img10 from '../assets/imgs/299.jpg';
 import Img11 from '../assets/imgs/31.jpg';
 import Img12 from '../assets/imgs/45.jpg';
 import Img13 from '../assets/imgs/bmw_before.jpg';
+import Img14 from '../assets/imgs/1.jpg';
+import Img15 from '../assets/imgs/2.jpg';
+import Img16 from '../assets/imgs/3.jpg';
+import Img17 from '../assets/imgs/5.jpg';
+import Img18 from '../assets/imgs/6.jpg';
+import Img19 from '../assets/imgs/7.jpg';
+import Img20 from '../assets/imgs/8.jpg';
+import Img21 from '../assets/imgs/9.jpg';
+import Img22 from '../assets/imgs/10.jpg';
+import Img23 from '../assets/imgs/12.jpg';
+import Img24 from '../assets/imgs/13.jpg';
+import Img25 from '../assets/imgs/15.jpg';
+import Img26 from '../assets/imgs/16.jpg';
 import Carousel from './Carousel';
 
 const Tech = () => {
-  const slides = [ Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10, Img11, Img12, Img13, Img1, Img2];
+  const slides = [ Img15, Img3, Img4, Img6, Img7, Img8, Img9, Img10, Img11, Img12, Img13, Img1, Img2, Img14, Img16, Img17, Img18, Img19, Img20, Img21, Img22, Img23, Img24, Img25, Img26];
   return (
     <>
       <motion.div variants={textVariant()}>
